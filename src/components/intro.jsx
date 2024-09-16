ef8a462f567b50a16be49d735aa5e0aa95ad60ab
DonJuan <armvnov1ch@gmail.com>
2024-09-16 16:19:34 +0500

    form style changed

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,7 +1,4 @@
-import React from "react";
-import Button from "@mui/material/Button";
-
-export const Intro = () => {
+import React from "react";export const Intro = () => {
    return (
       <div id="intro">
          <div className="container">
@@ -53,38 +50,60 @@ export const Intro = () => {
                   <div className="title">
                      <h1>Свяжитесь с нами</h1>
                   </div>
-                  <div className="form">
-                     <form name="contact" method="POST" data-netlify="true">
-                        {/* Скрытое поле для Netlify */}
-                        <input type="hidden" name="form-name" value="contact" />
+                  <form
+                     className="form"
+                     name="contact"
+                     method="POST"
+                     data-netlify="true"
+                  >
+                     {/* Скрытое поле для Netlify */}
+                     <input type="hidden" name="form-name" value="contact" />
 
-                        {/* Honeypot скрытое поле */}
-                        <div style={{ display: "none" }}>
-                           <input name="bot-field" />
-                        </div>
+                     {/* Honeypot скрытое поле */}
+                     <div style={{ display: "none" }}>
+                        <input name="bot-field" />
+                     </div>
 
-                        <div>
-                           <label htmlFor="name">Имя:</label>
-                           <input type="text" id="name" name="name" />
-                        </div>
-                        <div>
-                           <label htmlFor="lastname">Фамилия:</label>
-                           <input type="text" id="lastname" name="lastname" />
-                        </div>
-                        <div>
-                           <label htmlFor="phone">Номер:</label>
-                           <input type="tel" id="phone" name="phone" />
-                        </div>
-                        <div>
-                           <label htmlFor="comments">Комментарии:</label>
-                           <textarea id="comments" name="comments"></textarea>
-                        </div>
+                     <div className="intro-input">
+                        <label htmlFor="name"></label>
+                        <input
+                           type="text"
+                           id="name"
+                           name="name"
+                           placeholder="Имя"
+                        />
+                     </div>
+                     <div className="intro-input">
+                        <label htmlFor="lastname"></label>
+                        <input
+                           type="text"
+                           id="lastname"
+                           name="lastname"
+                           placeholder="Фамилия"
+                        />
+                     </div>
+                     <div className="intro-input">
+                        <label htmlFor="phone"></label>
+                        <input
+                           type="tel"
+                           id="phone"
+                           name="phone"
+                           placeholder="Телефон"
+                        />
+                     </div>
+                     <div className="intro-input">
+                        <label htmlFor="comments"></label>
+                        <textarea
+                           id="comments"
+                           name="comments"
+                           placeholder="Комментарии"
+                        ></textarea>
+                     </div>
 
-                        <Button type="submit" variant="contained">
-                           Отправить
-                        </Button>
-                     </form>
-                  </div>
+                     <button className="submit" type="submit">
+                        Отправить
+                     </button>
+                  </form>
                </div>
             </div>
          </div>
