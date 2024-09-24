de53dbc068588a7a4ae40bfdfdb4ef8fe6e21469
AIBIgroup <aibigroupinvest@gmail.com>
2024-09-24 08:40:39 +0000

    Update intro.jsx

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -52,59 +52,59 @@ export const Intro = () => {
                      <div className="title">
                         <h1>Свяжитесь с нами</h1>
                      </div>
-                     <form
-                        className="form"
-                        name="contact"
-                        method="POST"
-                        data-netlify="true"
-                     >
-                        {/* Скрытое поле для Netlify */}
-                        <input type="hidden" name="form-name" value="contact" />
-
-                        {/* Honeypot скрытое поле */}
-                        <div style={{ display: "none" }}>
-                           <input name="bot-field" />
-                        </div>
-
-                        <div className="intro-input">
-                           <label htmlFor="name"></label>
-                           <input
-                              type="text"
-                              id="name"
-                              name="name"
-                              placeholder="Имя"
-                           />
-                        </div>
-                        {/* <div className="intro-input">
-                           <label htmlFor="lastname"></label>
-                           <input
-                              type="text"
-                              id="lastname"
-                              name="lastname"
-                              placeholder="Фамилия"
-                           />
-                        </div> */}
-                        <div className="intro-input">
-                           <label htmlFor="phone"></label>
-                           <input
-                              type="tel"
-                              id="phone"
-                              name="phone"
-                              placeholder="Телефон"
-                           />
-                        </div>
-                        <div className="intro-input">
-                           <label htmlFor="comments"></label>
-                           <textarea
-                              id="comments"
-                              name="comments"
-                              placeholder="Комментарии"
-                           ></textarea>
-                        </div>
-
-                        <button className="submit" type="submit">
-                           Отправить
-                        </button>
+                     <form 
+                        className="form" 
+                        name="contact" 
+                        method="POST" 
+                        data-netlify="true" 
+                     > 
+                        {/* Скрытое поле для Netlify */} 
+                        <input type="hidden" name="form-name" value="contact" /> 
+ 
+                        {/* Honeypot скрытое поле */} 
+                        <div style={{ display: "none" }}> 
+                           <input name="bot-field" /> 
+                        </div> 
+ 
+                        <div className="intro-input"> 
+                           <label htmlFor="name"></label> 
+                           <input 
+                              type="text" 
+                              id="name" 
+                              name="name" 
+                              placeholder="Имя" 
+                           /> 
+                        </div> 
+                        {/* <div className="intro-input"> 
+                           <label htmlFor="lastname"></label> 
+                           <input 
+                              type="text" 
+                              id="lastname" 
+                              name="lastname" 
+                              placeholder="Фамилия" 
+                           /> 
+                        </div> */} 
+                        <div className="intro-input"> 
+                           <label htmlFor="phone"></label> 
+                           <input 
+                              type="tel" 
+                              id="phone" 
+                              name="phone" 
+                              placeholder="Телефон" 
+                           /> 
+                        </div> 
+                        <div className="intro-input"> 
+                           <label htmlFor="comments"></label> 
+                           <textarea 
+                              id="comments" 
+                              name="comments" 
+                              placeholder="Комментарии" 
+                           ></textarea> 
+                        </div> 
+ 
+                        <button className="submit" type="submit"> 
+                           Отправить 
+                        </button> 
                      </form>
                   </div>
                </div>
