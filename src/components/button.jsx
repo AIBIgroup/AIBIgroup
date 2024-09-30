ecc1d83f5e00a6ce018e3e97035bf4df676af363
AIBIgroup <aibigroupinvest@gmail.com>
2024-09-30 15:54:46 +0500

    Update button.jsx
    новый номер

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,7 +1,7 @@
 import React from "react";export const FixedButton = () => {
    return (
       <button className="fixed-button">
-         <a href="https://wa.me/77782576875">
+         <a href="https://wa.me/77774382855">
             <img src={`${process.env.PUBLIC_URL}/img/fixedbutton.png`} alt="" />
          </a>
       </button>
